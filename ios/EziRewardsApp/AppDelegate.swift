3741e22ac5836334e7aac179db16cc6ef220c380
Shurui <zhangshurui1989@gmail.com>
2025-02-18 18:44:47 +1030

    config firebase for ios

--- ios/EziRewardsApp/AppDelegate.swift
+++ ios/EziRewardsApp/AppDelegate.swift
@@ -2,17 +2,18 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import Firebase
 
 @main
 class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
     self.moduleName = "EziRewardsApp"
     self.dependencyProvider = RCTAppDependencyProvider()
 
     // You can add your custom initial props in the dictionary below.
     // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
